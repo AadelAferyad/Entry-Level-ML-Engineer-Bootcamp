5ee45d83116e5d886eff82b35bc6f4a5438a22d5
AadelAferyad <animevineu@gmail.com>
2025-03-13 04:30:20 +0000

    tqdm

--- Week00/ex06/loading.py
+++ Week00/ex06/loading.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 
 from tqdm import tqdm
+from time import sleep 
 
 def ft_progress(lst):
-    for i in range(lst):
+    for i in lst:
         yield i
 
 listy = range(1000)
 
 def main():
     for i in tqdm(ft_progress(listy)):
-        sleep(0.01)
+        sleep(0.25)
 
 main()
